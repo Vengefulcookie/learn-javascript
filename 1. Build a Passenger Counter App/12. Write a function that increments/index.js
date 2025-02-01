dc5e0620335e9edc516951a106f4cd202380c69d
Vengefulcookie <122882175+Vengefulcookie@users.noreply.github.com>
2025-02-01 21:30:50 +0200

    Update index.js

--- 1. Build a Passenger Counter App/12. Write a function that increments/index.js
+++ 1. Build a Passenger Counter App/12. Write a function that increments/index.js
@@ -1,9 +1,10 @@
 let lapsCompleted = 0
-
 // Create a function that increments the lapsCompleted variable with one
 // Run it three times
-
-
-
-
+function completedLaps() {
+  lapsCompleted = lapsCompleted + 1
+}  
+completedLaps();
+completedLaps();
+completedLaps();
 console.log(lapsCompleted)
