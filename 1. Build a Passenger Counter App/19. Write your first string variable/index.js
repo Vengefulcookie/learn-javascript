df03f8f3f2a8b5e2537b82c6c2837b452e29b7d4
Vengefulcookie <122882175+Vengefulcookie@users.noreply.github.com>
2025-02-02 15:59:17 +0200

    Update index.js

--- 1. Build a Passenger Counter App/19. Write your first string variable/index.js
+++ 1. Build a Passenger Counter App/19. Write your first string variable/index.js
@@ -2,5 +2,6 @@ let username = 'per"
   
 let message = "You have three new notifications"
 
-console.log(message + " " + username)
+let messageToUser = message + " " + username
 
+console.log(messageToUser)
