de25ddbb15beaf527b88c10550ad6b56360a5651
Vengefulcookie <122882175+Vengefulcookie@users.noreply.github.com>
2025-02-04 11:36:57 +0200

    Update index.js

--- 2. Practice time - part 1/06. Calculator challenge/index.js
+++ 2. Practice time - part 1/06. Calculator challenge/index.js
@@ -8,6 +8,11 @@ document.getElementById("num2-el").textContent = num2
 // Perform the given calculation using num1 and num2
 // Render the result of the calculation in the paragraph with id="sum-el"
 
+function add() {
+  = num1 + num2
+
+
+
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 
